66612dc8a05c51ffcbd0b58b39aa3a2e5059f0c5
ybilecenoglu <ybilecenoglu@gmail.com>
2023-05-04 01:09:51 +0300

    Repository sinifi eklendi ve veritabani islemleri yapildi

---
+++ TestProject/TestProject/Database/Product/ProductDal.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestProject.Models;
+
+namespace TestProject.Database.Product
+{
+    public class ProductDal : IProductDal
+    {
+        public void Add(Models.Product entity)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                TestProject.Models.Product product = new Models.Product();
+                product.ProductName = entity.ProductName;
+                product.UnitPrice = entity.UnitPrice;
+                product.QuantityPerUnit = entity.QuantityPerUnit;
+                product.UnitsInStock = entity.UnitsInStock;
+
+                context.Products.Add(product);
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(Models.Product product)
+        {
+            using (NorthwindContext context = new NorthwindContext() )
+            {
+                context.Products.Remove(product);
+                context.SaveChanges();
+            }
+        }
+
+        public Models.Product Get(Models.Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Models.Product> GetAll()
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                return context.Products.ToList();
+            }
+        }
+
+        public void Update(Models.Product entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TestProject/TestProject/Models/Shipper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestProject.Models;
+
+public partial class Shipper
+{
+    public int ShipperId { get; set; }
+
+    public string CompanyName { get; set; }
+
+    public string Phone { get; set; }
+
+    public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
+}
