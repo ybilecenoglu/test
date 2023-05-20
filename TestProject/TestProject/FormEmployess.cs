d9fe530859832193912cced11ddafab612ec07db
ygt3493 <ybilecenoglu@gmail.com>
2023-05-21 01:51:41 +0300

    formEmployess combobox filtereleme type kontrolü ile çözüldü

--- TestProject/TestProject/FormEmployess.cs
+++ TestProject/TestProject/FormEmployess.cs
@@ -1,19 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics.Metrics;
 using System.Drawing;
 using System.Drawing.Imaging;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using TestProject.Business;
 using TestProject.DAL.Employee;
 using TestProject.Models;
-using static Azure.Core.HttpHeader;
 
 namespace TestProject
 {
@@ -35,6 +26,7 @@ namespace TestProject
                 gdwEmployee.DataSource = await _employeeDAL.BindingList();
 
                 cbxRegion.DataSource = await _employeeDAL.GetRegions();
+                cbxRegion.SelectedIndex = -1;
                 cbxRegion.DisplayMember = "RegionDescription";
                 cbxRegion.ValueMember = "RegionID";
 
@@ -51,21 +43,21 @@ namespace TestProject
                 if (gdwEmployee.CurrentRow.Cells[0].Value != null && !string.IsNullOrEmpty(gdwEmployee.CurrentRow.Cells[0].Value.ToString()))
                 {
                     var employee = await _employeeDAL.GetAsync(Convert.ToInt32(gdwEmployee.CurrentRow.Cells[0].Value));
-                    tbxLastName.Text = gdwEmployee.CurrentRow.Cells[1].Value != null ? gdwEmployee.CurrentRow.Cells[1].Value.ToString() : "";
-                    tbxFirstName.Text = gdwEmployee.CurrentRow.Cells[2].Value != null ? gdwEmployee.CurrentRow.Cells[2].Value.ToString() : "";
-                    tbxTitle.Text = gdwEmployee.CurrentRow.Cells[3].Value != null ? gdwEmployee.CurrentRow.Cells[3].Value.ToString() : "";
-                    tbxTitleOfCourtesy.Text = gdwEmployee.CurrentRow.Cells[4].Value != null ? gdwEmployee.CurrentRow.Cells[4].Value.ToString() : "";
-                    dtpBirthDate.Value = gdwEmployee.CurrentRow.Cells[5].Value != null ? Convert.ToDateTime(gdwEmployee.CurrentRow.Cells[5].Value) : DateTime.Now;
-                    dtpHireDate.Value = gdwEmployee.CurrentRow.Cells[6].Value != null ? Convert.ToDateTime(gdwEmployee.CurrentRow.Cells[6].Value) : DateTime.Now;
-                    tbxAdress.Text = gdwEmployee.CurrentRow.Cells[7].Value != null ? gdwEmployee.CurrentRow.Cells[7].Value.ToString() : "";
-                    cbxCity.SelectedIndex = cbxCity.FindString(gdwEmployee.CurrentRow.Cells[8].Value != null ? gdwEmployee.CurrentRow.Cells[8].Value.ToString() : "");
-                    cbxRegion.SelectedIndex = cbxRegion.FindString(gdwEmployee.CurrentRow.Cells[9].Value != null ? gdwEmployee.CurrentRow.Cells[9].Value.ToString() : "");
-                    tbxPostalCode.Text = gdwEmployee.CurrentRow.Cells[10].Value != null ? gdwEmployee.CurrentRow.Cells[10].Value.ToString() : "";
-                    cbxCountry.SelectedIndex = cbxCountry.FindString(gdwEmployee.CurrentRow.Cells[11].Value != null ? gdwEmployee.CurrentRow.Cells[11].Value.ToString() : "");
-                    tbxPhone.Text = gdwEmployee.CurrentRow.Cells[12].Value != null ? gdwEmployee.CurrentRow.Cells[12].Value.ToString() : "";
-                    tbxExtension.Text = gdwEmployee.CurrentRow.Cells[13].Value != null ? gdwEmployee.CurrentRow.Cells[13].Value.ToString() : "";
-                    //pictureBox.Image = utilities.byteToImage(employee.Photo);
-                    rtbNote.Text = gdwEmployee.CurrentRow.Cells[15].Value != null ? gdwEmployee.CurrentRow.Cells[15].Value.ToString() : "";
+                    tbxLastName.Text = employee.LastName;
+                    tbxFirstName.Text = employee.FirstName;
+                    tbxTitle.Text = employee.Title;
+                    tbxTitleOfCourtesy.Text = employee.TitleOfCourtesy;
+                    dtpBirthDate.Value = employee.BirthDate != null ? employee.BirthDate.Value : DateTime.Now;
+                    dtpHireDate.Value = employee.HireDate != null ? employee.HireDate.Value : DateTime.Now;
+                    tbxAdress.Text = employee.Address;
+                    cbxCity.SelectedIndex = cbxCity.FindString(employee.City);
+                    cbxRegion.SelectedIndex = cbxRegion.FindString(employee.Region);
+                    tbxPostalCode.Text =employee.PostalCode;
+                    cbxCountry.SelectedIndex = cbxCountry.FindString(employee.Country != null ? employee.Country : "");
+                    tbxPhone.Text = employee.HomePhone;
+                    tbxExtension.Text = employee.Extension;
+                    pictureBox.Image = utilities.byteToImage(employee.Photo);
+                    rtbNote.Text = employee.Notes;
                 }
 
             });
@@ -123,7 +115,15 @@ namespace TestProject
 
         private async void cbxRegion_SelectedIndexChanged(object sender, EventArgs e)
         {
-            //cbxCity.DataSource = await _employeeDAL.GetTerritories(x => x.RegionId == );
+            //comboBox form ilk yüklemede object döndürdüğü bug type kontrolü ile çözüldü.
+            if (cbxRegion.SelectedValue != null && cbxRegion.SelectedValue.GetType() == typeof(int))
+            {
+                int RegionID = Convert.ToInt32(cbxRegion.SelectedValue);
+                cbxCity.DataSource = await _employeeDAL.GetTerritories(x => x.RegionId == RegionID);
+
+            }
         }
+
+
     }
 }
